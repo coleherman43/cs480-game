830a9874342c33639bc858a692d462e13e2296d8
lucyach <achesonlucy@gmail.com>
2026-04-21 21:13:33 -0700

    init robot and movement!

---
+++ Assets/Scripts/robotDroneMovement.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class robotDroneMovement : MonoBehaviour
+{
+    public float hoverHeight = 2.25f;
+    public float bobAmplitude = 0.3f;
+    public float bobSpeed = 1.75f;
+    public float moveDistance = 2f;
+    public float moveSpeed = 0.4f;
+
+    Vector3 basePosition;
+
+    void Awake()
+    {
+        basePosition = transform.position;
+    }
+
+    void Start()
+    {
+        ApplyHoverPosition();
+    }
+
+    void Update()
+    {
+        ApplyHoverPosition();
+    }
+
+    void ApplyHoverPosition()
+    {
+        float bobOffset = Mathf.Sin(Time.time * bobSpeed) * bobAmplitude;
+        float forwardOffset = Mathf.Sin(Time.time * moveSpeed) * moveDistance;
+        Vector3 moveOffset = transform.forward * forwardOffset;
+
+        transform.position = new Vector3(
+            basePosition.x + moveOffset.x,
+            hoverHeight + bobOffset,
+            basePosition.z + moveOffset.z
+        );
+    }
+}
+
