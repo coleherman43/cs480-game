6994612aaba8ff43d1656bb4d6bd046061230b64
WillJwitz <william.jurewitz12@gmail.com>
2026-05-04 16:49:25 -0700

    adapted tutorial text manager to Kobe's observer-listener system

--- Assets/Scripts/tutorialZone.cs
+++ Assets/Scripts/tutorialZone.cs
@@ -3,21 +3,12 @@ using UnityEngine;
 public class tutorialZone : MonoBehaviour
 {
     public string message;
-    private TextController tutorialManager;
 
-    void Start()
-    {
-        tutorialManager = GetComponentInParent<TextController>();
-        if (tutorialManager == null)
-        {
-            Debug.LogError(gameObject.name + "TextController not found in parent!");
-        }
-    }
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            tutorialManager.SetText(message);
+            GameEvents.OnZoneEnter?.Invoke(message);
         }
     }
 }
