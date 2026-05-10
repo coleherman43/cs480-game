1efd3fe6177257d1ca9b02ce7ed0570f36d907e4
WillJwitz <william.jurewitz12@gmail.com>
2026-05-10 10:31:17 -0700

    pressing E picks up nearby items, indicated by outline shader

--- Assets/Scripts/PickupItem.cs
+++ Assets/Scripts/PickupItem.cs
@@ -24,7 +24,6 @@ public class PickupItem : MonoBehaviour
     }
     public void toggleShader()
     {
-        Debug.Log("shader toggle attmept");
         rend.GetPropertyBlock(mpb);
 
         float current = mpb.GetFloat(ThicknessID);
--- Assets/Scripts/pickupDetection.cs
+++ Assets/Scripts/pickupDetection.cs
@@ -1,16 +1,42 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using System.Collections.Generic;
 
 public class pickupDetection : MonoBehaviour
 {
+    private List<GameObject> items = new List<GameObject>();
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            pickupNearby();
+        }
+    }
     private void OnTriggerEnter(Collider other)
     {
         PickupItem scr = other.GetComponent<PickupItem>();
         scr.toggleShader();
+
+        items.Add(other.gameObject);
     }
 
     private void OnTriggerExit(Collider other)
     {
         PickupItem scr = other.GetComponent<PickupItem>();
         scr.toggleShader();
+
+        items.Remove(other.gameObject);
+    }
+
+    private void pickupNearby()
+    {
+        Debug.Log("pickup attempt");
+        foreach(var i in items)
+        {
+            PickupItem scr = i.GetComponent<PickupItem>();
+            scr.OnPickup();
+        }
+        Debug.Log(items);
     }
 }
